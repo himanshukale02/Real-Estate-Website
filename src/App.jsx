64e7a9a41cb57cfc1829964410ae13283b530d8a
Himanshu Kale <himanshudkale@gmail.com>
2025-01-05 18:07:26 +0530

    Added Companies Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero"
+import Companies from "./components/Companies/Companies"
+import { Residencies } from "./components/Residencies/Residencies";
 import './App.css'
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,8 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
+      <Residencies />
     </div>
   );
 }
