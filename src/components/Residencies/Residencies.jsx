7d3d842f9f66a5a57e7f4f104a18fa06fb0f8e67
Himanshu Kale <himanshudkale@gmail.com>
2025-01-06 17:55:58 +0530

    added values section

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './Residencies.css'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import "swiper/css"
 import data from '../../utils/slider.json'
+import { sliderSettings } from '../../utils/common'
 export const Residencies = () => {
   return (
+
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
         <div className="r-head flexColStart">
           <span className='orangeText'>Best Choices</span>
           <span className='primaryText'>Popular Residencies</span>
         </div>
-        <Swiper>
+        <Swiper {...sliderSettings}>
+        
           {
             data.map((card, i) => (
               <SwiperSlide key={i}>
@@ -28,10 +32,22 @@ export const Residencies = () => {
 
                 </div>
               </SwiperSlide>
-            ))
-          }
+            ))}
+            <SliderButtons />
         </Swiper>
+        
       </div>
     </section>
+  )
+}
+export default Residencies;
+
+const SliderButtons = () => {
+  const swiper = useSwiper();
+  return (
+    <div className="flexCenter r-buttons">
+      <button onClick={()=>swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=>swiper.slideNext()}>&gt;</button>
+    </div>
   )
 }
